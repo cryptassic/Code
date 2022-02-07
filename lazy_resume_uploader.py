8e69d65b3d6cd346260ae79d84bb70e730343c71
Cryptassic <36624118+cryptassic@users.noreply.github.com>
2022-02-07 12:46:37 +0200

    Refactoring/Cleaning Code

--- lazy_resume_uploader.py
+++ lazy_resume_uploader.py
@@ -1,24 +1,11 @@
 
 import json
-import requests
+from requests import Response
+import requests 
 import configparser
-from dataclasses import dataclass
 
-from prep_payload import get_payload
-from make_form import FormMaker
+from prep_payload import PayloadMaterials, PayloadBuilder
 from solve_captcha import LOGGER
-
-
-@dataclass
-class PayloadMaterials:
-    target_pageurl: str
-    target_sitekey: str
-    encoded_resume: bytes
-    name: str
-    email: str
-    phone: str
-    profile_url: str
-    comment_section: str
 
 
 def load_resume(filepath):
@@ -28,60 +15,80 @@ def load_resume(filepath):
     return resume_data
 
 
-def make_payload(build_materials: PayloadMaterials):
-    fm = FormMaker(pageurl=build_materials.target_pageurl,
-                   sitekey=build_materials.target_sitekey)
+class LazyResume:
+    def __init__(self,debug=False):
+        self.debug_mode = debug
+        self.__oxy_config = None
+        self.__user_config = None
+        self.__cookies = None
+        self.__headers = None
 
-    form = fm.get_form(
-        resume=build_materials.encoded_resume,
-        name=build_materials.name,
-        email=build_materials.email,
-        phone=build_materials.phone,
-        profile_urls=build_materials.profile_url,
-        comments=build_materials.comment_section
-    )
+        self.__load_config()
 
-    payload = get_payload(form=form)
+    def __load_config(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
 
-    return payload
+        self.__oxy_config = config['Oxy']
+        self.__user_config = config['USER']
+
+        self.__cookies = json.loads(self.__oxy_config['cookies'])
+        self.__headers = json.loads(self.__oxy_config['headers'])
+
+        assert self.__cookies is not None, "Cookies not set"
+        assert self.__headers is not None, "Headers not set"
+
+    def __get_payload_materials(self) -> PayloadMaterials:
+        payload_build_materials = None
+        try:
+            payload_build_materials = PayloadMaterials(
+                target_pageurl=self.__oxy_config['pageurl'],
+                target_sitekey=self.__oxy_config['sitekey'],
+                encoded_resume=('Python_Resume.pdf', load_resume(
+                    'Resume.pdf'), "application/pdf"),
+                name=self.__user_config['name'],
+                email=self.__user_config['email'],
+                phone=self.__user_config['phone'],
+                profile_url=self.__user_config['profile_url'],
+                comment_section=self.__user_config['comment'])
+        except Exception as e:
+            LOGGER.error(e)
+        
+        return payload_build_materials
+    
+    def __handle_response(self,response:Response) -> bool:
+        if response.status_code == 200:
+            LOGGER.info("Success!")
+            return 1
+        else:
+            LOGGER.error("Failed!")
+            return 0
+    
+    def run(self):
+        payload_materials = self.__get_payload_materials()
+        
+        payload = PayloadBuilder.make_payload(build_materials=payload_materials)
+        
+        self.__headers.update({"Content-Type": payload.content_type})
+        
+        proxies = {}
+        target_url = self.__oxy_config['pageurl']
+        
+        if self.debug_mode:
+            proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+            target_url = "http://google.com"
+        
+        try:
+            response = requests.post(url=target_url, data=payload, headers=self.__headers, cookies=self.__cookies,proxies=proxies)
+        except Exception as e:
+            LOGGER.error(e)
+        else:
+            self.__handle_response(response)
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    oxy_config = config['Oxy']
-    user_config = config['USER']
-
-    cookies = json.loads(oxy_config['cookies'])
-    headers = json.loads(oxy_config['headers'])
-
-    payload_build_materials = PayloadMaterials(
-        target_pageurl=oxy_config['pageurl'],
-        target_sitekey=oxy_config['sitekey'],
-        encoded_resume=('Python_Resume.pdf', load_resume(
-            'Resume.pdf'), "application/pdf"),
-        name=user_config['name'],
-        email=user_config['email'],
-        phone=user_config['phone'],
-        profile_url=user_config['profile_url'],
-        comment_section=user_config['comment'])
-
-    payload = make_payload(build_materials=payload_build_materials)
-    headers.update({"Content-Type": payload.content_type})
-
-    # Burp Suite
-    # proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-    # response = requests.post(url="http://google.com", data=payload, headers=headers, cookies=cookies,proxies=proxies)
-
-    response = requests.post(
-        url=oxy_config['pageurl'], data=payload, headers=headers, cookies=cookies)
-
-    if response.status_code == 200:
-        LOGGER.info("Success!")
-    else:
-        LOGGER.error("Failed!")
-
+    lazy_dev = LazyResume()
+    lazy_dev.run()
 
 if __name__ == "__main__":
     main()
--- prep_payload.py
+++ prep_payload.py
@@ -1,10 +1,44 @@
 import random
 import string
-from make_form import Form
+from dataclasses import dataclass
+
+from make_form import Form,FormMaker
 from requests_toolbelt import MultipartEncoder
 
-def get_payload(form:Form):
-    boundary = '----WebKitFormBoundary' \
-           + ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+@dataclass
+class PayloadMaterials:
+    target_pageurl: str
+    target_sitekey: str
+    encoded_resume: bytes
+    name: str
+    email: str
+    phone: str
+    profile_url: str
+    comment_section: str
     
-    return MultipartEncoder(fields=form, boundary=boundary)
+class PayloadBuilder:
+       
+       @classmethod
+       def make_payload(cls,build_materials: PayloadMaterials):
+              fm = FormMaker(pageurl=build_materials.target_pageurl,
+                            sitekey=build_materials.target_sitekey)
+
+              form = fm.get_form(
+                     resume=build_materials.encoded_resume,
+                     name=build_materials.name,
+                     email=build_materials.email,
+                     phone=build_materials.phone,
+                     profile_urls=build_materials.profile_url,
+                     comments=build_materials.comment_section
+              )
+
+              payload = cls.__encode_form_to_webkit(form=form)
+
+              return payload
+
+       def __encode_form_to_webkit(self,form:Form):
+              boundary = '----WebKitFormBoundary' \
+                     + ''.join(random.sample(string.ascii_letters + string.digits, 16))
+              
+              return MultipartEncoder(fields=form, boundary=boundary)    
