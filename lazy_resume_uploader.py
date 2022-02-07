9331f667e5448dd7f5a7ccafded7ed1c4c97af85
Cryptassic <36624118+cryptassic@users.noreply.github.com>
2022-02-07 11:08:17 +0200

    Revert "Adding Cookies/Header support."
    This reverts commit 244218ad326075e5e7041248815eacf70c94d3eb.

--- lazy_resume_uploader.py
+++ lazy_resume_uploader.py
@@ -1,22 +1,8 @@
-from dataclasses import dataclass
 from make_form import FormMaker
-from prep_payload import get_payload
 import requests
 import configparser
-import json
+
 from solve_captcha import LOGGER
-
-@dataclass
-class PayloadMaterials:
-    target_pageurl:str
-    target_sitekey:str
-    encoded_resume:bytes
-    name:str
-    email:str
-    phone:str
-    profile_url:str
-    comment_section:str
-
 
 def load_resume(filepath):
     with open(filepath,'rb') as resume_file:
@@ -24,23 +10,6 @@ def load_resume(filepath):
     
     return resume_data
 
-
-def make_payload(build_materials:PayloadMaterials):
-    fm = FormMaker(pageurl=build_materials.target_pageurl,sitekey=build_materials.target_sitekey)
-
-    form = fm.get_form(
-        resume=build_materials.encoded_resume,
-        name=build_materials.name,
-        email=build_materials.email,
-        phone=build_materials.phone,
-        profile_urls=build_materials.profile_url,
-        comments=build_materials.comment_section
-    )
-    
-    payload = get_payload(form=form)
-    
-    return payload
-    
 def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -48,28 +17,29 @@ def main():
     oxy_config = config['Oxy']
     user_config = config['USER']
     
-    cookies = json.loads(oxy_config['cookies'])
-    headers = json.loads(oxy_config['headers'])
-    
-    payload_build_materials = PayloadMaterials(
-        target_pageurl = oxy_config['pageurl'],
-        target_sitekey = oxy_config['sitekey'],
-        encoded_resume =('Python_Resume.pdf', load_resume('Resume.pdf'), "application/pdf") ,
-        name = user_config['name'],
-        email = user_config['email'],
-        phone = user_config['phone'],
-        profile_url = user_config['profile_url'],
-        comment_section = user_config['comment'])
-    
-    
-    payload = make_payload(build_materials=payload_build_materials)
-    headers.update({"Content-Type": payload.content_type})
+    encoded_resume = load_resume('Resume.pdf')
+    name = user_config['name']
+    email = user_config['email']
+    phone = user_config['phone']
+    profile_url = user_config['profile_url']
+    comment_section = user_config['comment']
+
+    fm = FormMaker(pageurl=oxy_config['pageurl'],sitekey=oxy_config['sitekey'])
+
+    prebuilt_form = fm.get_form(
+        resume=encoded_resume,
+        name=name,
+        email=email,
+        phone=phone,
+        profile_urls=profile_url,
+        comments=comment_section
+    )
     
     #Burp Suite
     # proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-    # response = requests.post(url="http://google.com",data=payload,headers=headers,proxies=proxies,cookies=cookies)
+    # response = requests.post(url="http://google.com",files=prebuilt_form,proxies=proxies)
     
-    response = requests.post(url=oxy_config['pageurl'],headers=headers,data=payload,cookies=cookies)
+    response = requests.post(url=oxy_config['pageurl'],files=prebuilt_form)
     
     if response.status_code == 200:
         LOGGER.info("Success!")
--- prep_payload.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-from make_form import Form
-from requests_toolbelt import MultipartEncoder
-
-def get_payload(form:Form):
-    boundary = '----WebKitFormBoundary' \
-           + ''.join(random.sample(string.ascii_letters + string.digits, 16))
-    return MultipartEncoder(fields=form, boundary=boundary)
